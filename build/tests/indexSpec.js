ea7b407d888e75da2f2a91082583e6c9454d6b7b
nhnam <37695601+hoai97nam@users.noreply.github.com>
2023-07-02 17:21:57 +0700

    clear build files

--- build/tests/indexSpec.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-describe('Sample Test Suite', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
-  });
-});
--- build/utilities/logger.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const logger = (req, res, next) => {
-    const url = req.baseUrl;
-    console.log(`${new Date()}-${url} was called`);
-    next();
-};
-exports.default = logger;
